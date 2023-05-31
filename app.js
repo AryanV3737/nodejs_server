364e34392442a4ae161af95244c73781528ae5bc
AryanV3737 <131846909+AryanV3737@users.noreply.github.com>
2023-05-31 16:38:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'Doctor_Appointment'
+    user: 'dbuser101',
+    password: 'Dbpass@1',
+    database: 'doctor_appointment'
 });
 
 app.use(express.json());
